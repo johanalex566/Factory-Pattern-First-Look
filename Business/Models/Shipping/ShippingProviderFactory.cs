1e820a37e565830d131143e6b0469bd346564426
johanalex566 <johanalex566@gmail.com>
2022-12-21 15:59:24 -0500

    Add ShippingProviderFactory, remove fron Shopping cart to make provider reusable

---
+++ Business/Models/Shipping/ShippingProviderFactory.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Factory_Pattern_First_Look.Business.Models.Shipping
+{
+    public class ShippingProviderFactory
+    {
+
+        public static ShippingProvider CreateChippingProvider(string country)
+        {
+            #region Create Shipping Provider
+            ShippingProvider shippingProvider;
+
+            if (country == "Australia")
+            {
+                #region Australia Post Shipping Provider
+                var shippingCostCalculator = new ShippingCostCalculator(
+                    internationalShippingFee: 250,
+                    extraWeightFee: 500
+                )
+                {
+                    ShippingType = ShippingType.Standard
+                };
+
+                var customsHandlingOptions = new CustomsHandlingOptions
+                {
+                    TaxOptions = TaxOptions.PrePaid
+                };
+
+                var insuranceOptions = new InsuranceOptions
+                {
+                    ProviderHasInsurance = false,
+                    ProviderHasExtendedInsurance = false,
+                    ProviderRequiresReturnOnDamange = false
+                };
+
+                shippingProvider = new AustraliaPostShippingProvider("CLIENT_ID",
+                    "SECRET",
+                    shippingCostCalculator,
+                    customsHandlingOptions,
+                    insuranceOptions);
+                #endregion
+            }
+            else if (country == "Sweden")
+            {
+                #region Swedish Postal Service Shipping Provider
+                var shippingCostCalculator = new ShippingCostCalculator(
+                    internationalShippingFee: 50,
+                    extraWeightFee: 100
+                )
+                {
+                    ShippingType = ShippingType.Express
+                };
+
+                var customsHandlingOptions = new CustomsHandlingOptions
+                {
+                    TaxOptions = TaxOptions.PayOnArrival
+                };
+
+                var insuranceOptions = new InsuranceOptions
+                {
+                    ProviderHasInsurance = true,
+                    ProviderHasExtendedInsurance = false,
+                    ProviderRequiresReturnOnDamange = false
+                };
+
+                shippingProvider = new SwedishPostalServiceShippingProvider("API_KEY",
+                    shippingCostCalculator,
+                    customsHandlingOptions,
+                    insuranceOptions);
+                #endregion
+            }
+            else
+            {
+                throw new NotSupportedException("No shipping provider found for origin country");
+            }
+            #endregion
+
+            return shippingProvider;
+
+        }
+
+
+    }
+}
--- Business/ShoppingCart.cs
+++ Business/ShoppingCart.cs
@@ -15,73 +15,7 @@ namespace Factory_Pattern_First_Look.Business
 
         public string Finalize()    
         {
-            #region Create Shipping Provider
-            ShippingProvider shippingProvider;
-
-            if (order.Sender.Country == "Australia")
-            {
-                #region Australia Post Shipping Provider
-                var shippingCostCalculator = new ShippingCostCalculator(
-                    internationalShippingFee: 250,
-                    extraWeightFee: 500
-                )
-                {
-                    ShippingType = ShippingType.Standard
-                };
-
-                var customsHandlingOptions = new CustomsHandlingOptions
-                {
-                    TaxOptions = TaxOptions.PrePaid
-                };
-
-                var insuranceOptions = new InsuranceOptions
-                {
-                    ProviderHasInsurance = false,
-                    ProviderHasExtendedInsurance = false,
-                    ProviderRequiresReturnOnDamange = false
-                };
-
-                shippingProvider = new AustraliaPostShippingProvider("CLIENT_ID",
-                    "SECRET",
-                    shippingCostCalculator,
-                    customsHandlingOptions,
-                    insuranceOptions);
-                #endregion
-            }
-            else if (order.Sender.Country == "Sweden")
-            {
-                #region Swedish Postal Service Shipping Provider
-                var shippingCostCalculator = new ShippingCostCalculator(
-                    internationalShippingFee: 50,
-                    extraWeightFee: 100
-                )
-                {
-                    ShippingType = ShippingType.Express
-                };
-
-                var customsHandlingOptions = new CustomsHandlingOptions
-                {
-                    TaxOptions = TaxOptions.PayOnArrival
-                };
-
-                var insuranceOptions = new InsuranceOptions
-                {
-                    ProviderHasInsurance = true,
-                    ProviderHasExtendedInsurance = false,
-                    ProviderRequiresReturnOnDamange = false
-                };
-
-                shippingProvider = new SwedishPostalServiceShippingProvider("API_KEY",
-                    shippingCostCalculator,
-                    customsHandlingOptions,
-                    insuranceOptions);
-                #endregion
-            }
-            else
-            {
-                throw new NotSupportedException("No shipping provider found for origin country");
-            }
-            #endregion
+            var shippingProvider = ShippingProviderFactory.CreateChippingProvider(order.Sender.Country);
 
             order.ShippingStatus = ShippingStatus.ReadyForShippment;
 
